86ab652746bd6fb87d1d53ba092b42b4f42166c8
Juber Ahammad Shaik <juber.shaik@bilvantis.io>
2025-12-02 12:12:35 +0530

    deleted unused files and added nativewind

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const index = () => {
+  return (
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-3xl font-pregular">index</Text>
+    </View>
+  )
+}
+
+export default index
