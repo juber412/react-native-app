7d0eff1486eec1b7998589d9b0e3ce2269712462
juber412 <shaikjuber412@gmail.com>
2025-12-03 10:12:43 +0530

    added bottom tabs

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'expo-router'
 import { Text, View } from 'react-native'
 
 const index = () => {
   return (
     <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-3xl font-pregular">index</Text>
+      <Text className="text-3xl font-pregular">React Native App</Text>
+      <Link href="/home">
+        <Text className="font-psemibold">Go To Home</Text>
+      </Link>
     </View>
   )
 }
